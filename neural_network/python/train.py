1f9b2f65f5ac60afa987e9d7e7293d8c4a6a9f31
ZiheLiu <ziheliu1024@gmail.com>
2018-10-31 20:46:05 +0800

    Set hyper params with aguments instead of constants

--- neural_network/python/train.py
+++ neural_network/python/train.py
@@ -7,16 +7,20 @@ from utils.shell_args import SHELL_ARGS
 
 
 class Train(object):
-    def __init__(self):
+    def __init__(self, hidden_size, batch_size, learning_rate):
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+
         self.data = Data(SHELL_ARGS.prefix)
         self.model = NeuralNetwork(self.data.input_size(),
-                                   constants.HIDDEN_SIZE,
+                                   self.hidden_size,
                                    self.data.output_size(),
-                                   constants.BATCH_SIZE)
+                                   self.batch_size)
 
     def _calc_accurate(self, batches_func, batches_sum):
         accurate = 0
-        for test_source_batch, test_target_batch in batches_func(constants.BATCH_SIZE):
+        for test_source_batch, test_target_batch in batches_func(self.batch_size):
             predictions = self.model.predict(test_source_batch)
             mask = predictions == test_target_batch
             accurate += np.mean(mask)
@@ -24,24 +28,27 @@ class Train(object):
         return accurate
 
     def train(self):
+        train_batches_sum = self.data.train_batches_sum(self.batch_size)
+        eval_batches_sum = self.data.eval_batches_sum(self.batch_size)
+        test_batches_sum = self.data.test_batches_sum(self.batch_size)
+
         pre_train_loss_value = 1000
         pre_eval_loss_value = 1000
         increase_times = 0
         for epoch_i in range(constants.EPOCHS):
             # self.data.reshuffle_train_data()
             cur_train_loss_value = 0
-            cur_eval_loss_value = 0
-
-            for train_source_batch, train_target_batch in self.data.train_batches(constants.BATCH_SIZE):
-                loss_value = self.model.train(train_source_batch, train_target_batch, constants.LEARNING_RATE)
+            for train_source_batch, train_target_batch in self.data.train_batches(self.batch_size):
+                loss_value = self.model.train(train_source_batch, train_target_batch, self.learning_rate)
                 cur_train_loss_value += loss_value
+            cur_train_loss_value /= train_batches_sum
 
-            for eval_source_batch, eval_target_batch in self.data.eval_batches(constants.BATCH_SIZE):
+            cur_eval_loss_value = 0
+            for eval_source_batch, eval_target_batch in self.data.eval_batches(self.batch_size):
                 loss_value = self.model.loss(eval_source_batch, eval_target_batch)
                 cur_eval_loss_value += loss_value
+            cur_eval_loss_value /= eval_batches_sum
 
-            cur_train_loss_value /= self.data.train_batches_sum(constants.BATCH_SIZE)
-            cur_eval_loss_value /= self.data.eval_batches_sum(constants.BATCH_SIZE)
             print('Epoch_i: %d, train loss: %.6f, eval loss: %.6f' %
                   (epoch_i, cur_train_loss_value, cur_eval_loss_value))
 
@@ -53,12 +60,14 @@ class Train(object):
             pre_train_loss_value = cur_train_loss_value
             pre_eval_loss_value = cur_eval_loss_value
 
-        train_accurate = self._calc_accurate(self.data.train_batches, self.data.train_batches_sum(constants.BATCH_SIZE))
-        eval_accurate = self._calc_accurate(self.data.eval_batches, self.data.eval_batches_sum(constants.BATCH_SIZE))
-        test_accurate = self._calc_accurate(self.data.test_batches, self.data.test_batches_sum(constants.BATCH_SIZE))
+        train_accurate = self._calc_accurate(self.data.train_batches, train_batches_sum)
+        eval_accurate = self._calc_accurate(self.data.eval_batches, eval_batches_sum)
+        test_accurate = self._calc_accurate(self.data.test_batches, test_batches_sum)
 
         print('train_accurate: %.6f, eval_accurate: %.6f, test_accurate: %.6f' % (train_accurate, eval_accurate, test_accurate))
 
 
 if __name__ == '__main__':
-    Train().train()
+    Train(hidden_size=constants.HIDDEN_SIZE,
+          batch_size=constants.BATCH_SIZE,
+          learning_rate=constants.LEARNING_RATE).train()
