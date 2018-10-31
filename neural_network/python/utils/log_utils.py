3e0f5a5556af9790d2e28d475b7b05a0f5bc757d
ZiheLiu <ziheliu1024@gmail.com>
2018-10-31 23:10:52 +0800

    Add log_utils

--- neural_network/python/utils/log_utils.py
+++ neural_network/python/utils/log_utils.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import constants
+from utils import file_utils
 from utils.shell_args import SHELL_ARGS
 
 formatter = '%(asctime)s - %(levelname)s - %(message)s'
@@ -9,6 +10,7 @@ formatter = '%(asctime)s - %(levelname)s - %(message)s'
 
 data_name = os.path.split(SHELL_ARGS.prefix)[-1]
 logging_filename = os.path.join(constants.OUTPUT_DIR, data_name, 'logs')
+file_utils.check_and_create_dir(logging_filename)
 
 logging.basicConfig(filename=logging_filename,
                     level=logging.INFO,
